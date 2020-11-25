7dc55784d1cd6dc95e596666a452f0ddc0a103ba
path1heaven <parth1hell@gmail.com>
2020-11-25 09:49:28 +0545

    initial project creation with required dependencies

---
+++ src/main/java/com/cotiviti/erestaurantauth/ERestaurantAuthApplication.java
@@ -0,0 +1,13 @@
+package com.cotiviti.erestaurantauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERestaurantAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERestaurantAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotiviti/erestaurantauth/ERestaurantAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotiviti.erestaurantauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERestaurantAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
